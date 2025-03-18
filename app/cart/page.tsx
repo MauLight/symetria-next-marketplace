5de0f88ce7fc89d587d05108b74b5bf222eb951d
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-03-18 11:04:05 -0300

    feat: add point of return from cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,39 +1,27 @@
 import React from 'react'
 import CartList from './components/cart-list'
 import CartSummary from './components/cart-summary'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 export default function Page() {
 
     return (
-        <div className='w-full h-screen flex justify-center items-start mt-[60px]'>
+        <div className='w-full h-full flex justify-center items-start mt-[60px]'>
             <div className="w-full max-w-[1440px] flex flex-col gap-y-20 py-10">
                 <div>
-                    <h1 className='text-[2.5rem] leading-10'>Your Cart</h1>
+                    <div className="flex justify-between">
+                        <h1 className='text-[2.5rem] leading-10'>Your Cart</h1>
+                        <Link href={'/'}>
+                            <XMarkIcon className='w-6 h-6 hover:rotate-45 transition-all duration-300 ease-in-out hover:text-red-500' />
+                        </Link>
+                    </div>
                     <p className='text-[1.5rem] text-[#080808]'>Your products are not reserved until payment is complete.</p>
                 </div>
                 <div className="grid grid-cols-3 gap-x-5">
 
                     <CartList />
                     <CartSummary />
-
-                    <div className="col-span-1 flex flex-col gap-y-5 border border-[#dfdfdf] shadow-xl shadow-gray-50 rounded-[6px] p-5">
-                        <h1 className='text-[1.5rem]'>Summary</h1>
-                        <div>
-                            <div className='flex justify-between'>
-                                <p>0 Products</p>
-                                <p>$0.00</p>
-                            </div>
-                            <div className='flex justify-between'>
-                                <p>Total amount</p>
-                                <p>$0.00</p>
-                            </div>
-                            <div className='flex justify-between'>
-                                <p>Includes</p>
-                                <p>$0.00 VAT</p>
-                            </div>
-                        </div>
-                        <button className="text-[#ededed] w-full py-2 mt-5 rounded-[6px] border border-[#080808] hover:bg-transparent bg-black hover:text-[#080808] transition-all duration-300">Checkout</button>
-                    </div>
 
                 </div>
             </div>
