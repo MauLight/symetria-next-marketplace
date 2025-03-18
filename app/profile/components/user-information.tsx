e28a30960bf757bdc4c9f2780a662f3800b5aeb2
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-03-18 05:16:36 -0300

    feat: finish profile UI
    added editing form, wishlist placeholder

--- app/profile/components/user-information.tsx
+++ app/profile/components/user-information.tsx
@@ -1,8 +1,96 @@
-export default async function UserInformation() {
+'use client'
 
+import { CloudArrowUpIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+import { motion } from "motion/react";
 
+export default function UserInformation({ firstname, lastname, email, phone, street, city, country, zipcode }: {
+    firstname: string;
+    lastname: string;
+    email: string;
+    phone: string;
+    street: string;
+    city: string;
+    country: string;
+    zipcode: string;
+}
+) {
+
+    const [editing, setEditing] = useState(false)
+
+    useEffect(() => {
+        console.log(editing)
+    }, [editing])
 
     return (
-        <div>Something</div>
+        <div className="col-span-1">
+
+            {
+                editing ? (
+                    <motion.div
+                        key={1}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.5 }}
+                        className="flex flex-col gap-y-8 p-5 text-[#ededed]">
+                        <div className="flex gap-x-5 items-center">
+                            <input type="text" placeholder={`${firstname} ${lastname}`} className="w-full h-[32px] px-5 border border-[#292929] rounded-[6px]" />
+                            <button onClick={() => { setEditing(!editing) }}>
+                                <CloudArrowUpIcon className="w-5 h-5 hover:text-green-500 transition-color duration-300" />
+                            </button>
+                        </div>
+                        <div className="flex flex-col gap-y-2 pr-10">
+                            <h2 className="uppercase text-[0.8rem]">Contact</h2>
+                            <div className="flex flex-col gap-y-3">
+                                <input type="text" placeholder={email} className="w-full h-[32px] px-5 border border-[#292929] rounded-[6px]" />
+                                <input type="text" placeholder={phone} className="w-full h-[32px] px-5 border border-[#292929] rounded-[6px]" />
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-y-2 pr-10">
+                            <h2 className="uppercase text-[0.8rem]">Address</h2>
+                            <div className="flex flex-col gap-y-3">
+                                <input type="text" placeholder={street} className="w-full h-[32px] px-5 border border-[#292929] rounded-[6px]" />
+                                <input type="text" placeholder={city} className="w-full h-[32px] px-5 border border-[#292929] rounded-[6px]" />
+                                <input type="text" placeholder={country} className="w-full h-[32px] px-5 border border-[#292929] rounded-[6px]" />
+                                <input type="text" placeholder={zipcode} className="w-full h-[32px] px-5 border border-[#292929] rounded-[6px]" />
+                            </div>
+                        </div>
+                    </motion.div>
+                )
+                    :
+                    (
+                        <motion.div
+                            key={2}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 0.5 }}
+                            className="flex flex-col gap-y-8 p-5 text-[#ededed]">
+                            <div className="flex gap-x-5 items-center">
+                                <h1 className="text-[1.5rem]">{`${firstname} ${lastname}`}</h1>
+                                <button onClick={() => { setEditing(!editing) }}>
+                                    <PencilSquareIcon className="w-5 h-5 hover:text-indigo-500 transition-color duration-300" />
+                                </button>
+                            </div>
+                            <div className="flex flex-col gap-y-2">
+                                <h2 className="uppercase text-[0.8rem]">Contact</h2>
+                                <div>
+                                    <p>{email}</p>
+                                    <p>{phone}</p>
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-y-2">
+                                <h2 className="uppercase text-[0.8rem]">Address</h2>
+                                <div>
+                                    <p>{street}</p>
+                                    <p>{city}</p>
+                                    <p>{country}</p>
+                                    <p>{zipcode}</p>
+                                </div>
+                            </div>
+                        </motion.div>
+                    )
+            }
+
+        </div>
     )
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,9 @@
-import User from "@/api/models/User";
-import dbConnect from "@/api/mongoose";
-import { auth } from "@/auth";
-import mongoose from "mongoose";
+import User from "@/api/models/User"
+import dbConnect from "@/api/mongoose"
+import { auth } from "@/auth"
+import mongoose from "mongoose"
+import UserInformation from "./components/user-information"
+import { ShoppingCartIcon } from "@heroicons/react/24/outline"
 
 export default async function Page() {
 
@@ -21,17 +23,49 @@ export default async function Page() {
     const user = await getUser()
 
     return (
-        <div className='w-[1440px] border border-white min-h-screen flex flex-col pt-[100px]'>
-            {
-                user && (
-                    <>
-                        <div>
-                            <h1 className='text-[2.5rem] leading-10 text-[#ededed]'>Profile</h1>
-                            <p className='text-[1.5rem] text-[#a1a1a1]'>Edit your personal information and wishlist.</p>
-                        </div>
-                    </>
-                )
-            }
+        <div className='w-[1440px] h-full flex flex-col pt-[150px]'>
+
+            <section className="flex flex-col gap-y-10">
+                <div>
+                    <h1 className='text-[2.5rem] leading-12 text-[#ededed]'>Profile</h1>
+                    <p className='text-[1rem] text-[#a1a1a1]'>Edit your personal information and wishlist.</p>
+                </div>
+                <div className="w-full grid grid-cols-3">
+                    <UserInformation
+                        firstname={user.firstname}
+                        lastname={user.lastname}
+                        email={user.email}
+                        phone={user.phone}
+                        street={user.street}
+                        city={user.city}
+                        country={user.country}
+                        zipcode={user.zipcode}
+                    />
+                    <div className="col-span-2 flex flex-col gap-y-8 py-5 pr-5 pl-10 text-[#ededed] border-l border-[#292929]">
+                        <h1 className="text-[1.5rem]">Wishlist</h1>
+                        <>
+                            {
+                                Array.from({ length: 3 }).map((_, i) => (
+                                    <div key={i} className="w-full h-[120px] flex items-center justify-between py-2 px-5 border border-[#292929] rounded-[6px]">
+                                        <div className="flex gap-x-5">
+                                            <div className="w-[100px] h-[100px] border border-[#292929]"></div>
+                                            <div className="flex flex-col justify-center">
+                                                <h1 className="text-[1.2rem]">Title</h1>
+                                                <p className="text-[#a1a1a1]">Price</p>
+                                            </div>
+                                        </div>
+                                        <button className="flex flex-col items-center text-[#a1a1a1] hover:text-green-500 transition-color duration-300">
+                                            <ShoppingCartIcon className="w-8 h-8" />
+                                            <small>Add to Cart</small>
+                                        </button>
+                                    </div>
+                                ))
+                            }
+                        </>
+                    </div>
+                </div>
+            </section>
+
         </div>
     )
 }
