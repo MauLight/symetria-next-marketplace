b15d0f5a3ffa0291eae2de6c1cfa88a149e0a0a5
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-03-25 03:35:41 -0300

    feat: add padding on mobile
    in signTopbar

--- app/ui/signTopbar.tsx
+++ app/ui/signTopbar.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function SignTopbar() {
     return (
         <div className='absolute top-0 right-0 w-full flex justify-center'>
-            <div className="w-full max-w-[1440px] h-[60px] flex items-center justify-end">
+            <div className="w-full max-w-[1440px] max-sm:px-5 h-[60px] flex items-center justify-end">
                 <Link className='text-sym-text-secondary hover:text-indigo-500 transition-color duration-300 text-[1rem]' href={'/home'}>Back to home</Link>
             </div>
         </div>
